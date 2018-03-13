83e09924175ff0d164d889dcb0dff96af75a4e42
Jeffery Shivers <jeffery.shivers@precisioneffect.com>
2018-03-13 17:54:40 -0400

    decouple tests and hide webpack hints

--- karma.conf.js
+++ karma.conf.js
@@ -62,6 +62,9 @@ module.exports = function(config) {
     concurrency: Infinity,
 
     webpack: {
+      performance: {
+        hints: false
+      },
       module: {
         rules: [
           {
